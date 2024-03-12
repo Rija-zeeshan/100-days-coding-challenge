204aba67188248aa866975806684a120d8241ba7
Rija-zeeshan <161062396+Rija-zeeshan@users.noreply.github.com>
2024-03-13 03:37:08 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,12 @@
+
+//*Question 2:* Personal Message: Store a person’s name in a variable and print a message to them, like “Hello Rija, would you like to learn some TypeScript today?”
+
+let personName="Rija";
+console.log("Hello",personName," would you like to learn some typescript today?");
+
+//Question 3:* Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase
+
+let Name="Rija";
+console.log(Name.toLowerCase());
+console.log(Name.toUpperCase());
+console.log(Name.charAt(0).toUpperCase()+Name.slice(1).toLowerCase());
